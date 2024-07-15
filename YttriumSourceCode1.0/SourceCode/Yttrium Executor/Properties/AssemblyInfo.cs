5dd3aafb8752e8c154c51d9181b188a54045f449
imclarenzz <clarenzmendiola33@gmail.com>
2024-07-15 18:51:44 -0300

    Yttrium Source V 1.0

---
+++ YttriumSourceCode1.0/SourceCode/Yttrium Executor/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Yttrium Executor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yttrium Executor")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("111df6d7-8aa4-4e9d-89b0-217b9bb5acfe")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
